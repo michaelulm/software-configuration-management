7a999921b95f5de843efcc8101ae506e79d432b6
Michael Ulm <michael.ulm@fh-joanneum.at>
2019-04-01 21:00:33 +0200

    added extended example BubbleSort for later Exercises

---
+++ test-automation/BubbleSort/src/test/java/at/fhj/iit/BubbleSortTest.java
@@ -0,0 +1,16 @@
+package at.fhj.iit;
+
+import org.junit.Test;
+
+public class BubbleSortTest
+{
+
+    @Test
+    public void testSort()
+    {
+        // TODO
+    }
+
+
+    // TODO other Tests
+}
